0638a7002833053588bf78d35a4bbf55df2da90b
uvsq21807189 <mouwafak.houssein.ahmed@gmail.com>
2020-05-08 08:19:24 +0200

    finalisation de la classe GroupeDAOJDBC et suite de AnnuaireDAOJDBC

--- src/main/java/JDBC/AnnuaireDAOJDBC.java
+++ src/main/java/JDBC/AnnuaireDAOJDBC.java
@@ -27,5 +27,185 @@ public class AnnuaireDAOJDBC extends DAO<Annuaire> {
         }
     }
 
+    private static boolean tableExists(final String name, final Connection conn)
+            throws SQLException {
+        DatabaseMetaData dbmd = conn.getMetaData();
+        ResultSet rs = dbmd.getTables(null, null, name.toUpperCase(), null);
+        if (rs.next()) {
+            return true;
+        }
+        return false;
+    }
+    
+    public static void createTables(final Connection conn) throws SQLException {
+        TelephoneDAOJDBC.createTable(conn);
+        PersonnelDAOJDBC.createTables(conn);
+        GroupeDAOJDBC.createTables(conn);
+        try (Statement stmt = conn.createStatement()) {
+            if (!tableExists("annuaire", conn)) {
+                stmt.executeUpdate("Create table annuaire ("
+                        + "id int primary key, " + "racine_personnel int, "
+                        + "racine_groupe int,"
+                        + "check ((racine_personnel is null and"
+                        + " racine_groupe is not null) or"
+                        + "(racine_personnel is not null and"
+                        + " racine_groupe is null)),"
+                        + "foreign key (racine_personnel)"
+                        + " references personnel(id),"
+                        + "foreign key (racine_groupe)"
+                        + " references groupe(id))");
+            }
+        }
+    }
 
+    @Override
+    public Annuaire create(final Annuaire obj) {
+        try {
+            createTables(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        Annuaire created = null;
+        try {
+            connection.setAutoCommit(false);
+            try (PreparedStatement insertAnnuaire = connection.prepareStatement(
+                    "INSERT into annuaire values " + "(?, ?, ?)")) {
+                Composant racine = obj.getRacine();
+                if (racine instanceof Personnel) {
+                    Personnel p = (Personnel) racine;
+                    try (PreparedStatement selectPersonnel =
+                            connection.prepareStatement(
+                                    "SELECT * FROM personnel WHERE id = ?")) {
+                        selectPersonnel.setInt(1, p.getId());
+                        try (ResultSet rs = selectPersonnel.executeQuery()) {
+                            if (!rs.next()) {
+                                PersonnelDAOJDBC.insert(p, connection);
+                            } else {
+                                System.err.println("Personnel " + p.getId()
+                                + " existe déjà");
+                            }
+                        }
+                    }
+                    insertAnnuaire.setNull(3, Types.INTEGER);
+                    insertAnnuaire.setInt(2, p.getId());
+                } else if (racine instanceof Groupe) {
+                    Groupe g = (Groupe) racine;
+                    try (PreparedStatement selectGroupe =
+                            connection.prepareStatement(
+                                    "SELECT * FROM groupe WHERE id = ?")) {
+                        selectGroupe.setInt(1, g.getId());
+                        try (ResultSet rs = selectGroupe.executeQuery()) {
+                            if (!rs.next()) {
+                                GroupeDAOJDBC.insert(g, connection);
+                            } else {
+                                System.err.println("Groupe " + g.getId()
+                                + " existe déjà.");
+                            }
+                        }
+                    }
+                }
+                
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            try {
+                connection.rollback();
+                System.err.println("L'insertion de l'annuaire " + obj.getId() + " a échouée.");
+            } catch (SQLException e1) {
+                System.err.println(e1.getMessage());
+            }
+        }
+        return created;
+    }
+
+    @Override
+    public Annuaire find(final String id) {
+        Annuaire a = null;
+        try (PreparedStatement selectAnnuaire = connection
+                .prepareStatement("SELECT * FROM annuaire WHERE id = ?")) {
+            selectAnnuaire.setInt(1, Integer.parseInt(id));
+            try (ResultSet rs = selectAnnuaire.executeQuery()) {
+                if (rs.next()) {
+                    int idRacine = rs.getInt("racine_personnel");
+                    if (!rs.wasNull()) {
+                        Personnel p =
+                                PersonnelDAOJDBC.read(idRacine, connection);
+                        a = new Annuaire(rs.getInt("id"), p);
+                    } else {
+                        idRacine = rs.getInt("racine_groupe");
+                        if (!rs.wasNull()) {
+                            Groupe g = GroupeDAOJDBC.read(idRacine, connection);
+                            a = new Annuaire(rs.getInt("id"), g);
+                        }
+                    }
+                } else {
+                    System.err.println("Aucun annuaire avec l'identifiant" + id);
+                    return null;
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return a;
+    }
+
+    @Override
+    public Annuaire update(final Annuaire obj) {
+        Annuaire updated = null;
+        try {
+            connection.setAutoCommit(false);
+            try (PreparedStatement updateAnnuaire = connection.prepareStatement(
+                    "UPDATE annuaire SET racine_groupe = ?, racine_personnel = ? "
+                            + "WHERE id = ?")) {
+                updateAnnuaire.setInt(3, obj.getId());
+                Composant racine = obj.getRacine();
+                if (racine instanceof Personnel) {
+                    Personnel p = (Personnel) racine;
+                    PersonnelDAOJDBC.modify(p, connection, true);
+                    updateAnnuaire.setNull(1, Types.INTEGER);
+                    updateAnnuaire.setInt(2, p.getId());
+                } else if (racine instanceof Groupe) {
+                    Groupe g = (Groupe) racine;
+                    GroupeDAOJDBC.modify(g, connection, true);
+                    updateAnnuaire.setNull(2, Types.INTEGER);
+                    updateAnnuaire.setInt(1, g.getId());
+                }
+                updateAnnuaire.execute();
+                connection.commit();
+                updated = obj;
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            try {
+                connection.rollback();
+                System.err.println("La mise à jour de l'annuaire" + obj.getId() + " a échouée.");
+            } catch (SQLException e1) {
+                System.err.println(e1.getMessage());
+            }
+        } 
+        return updated;
+    }
+
+    @Override
+    public void delete(final Annuaire obj) {
+        try {
+            connection.setAutoCommit(false);
+            try (PreparedStatement delete = connection
+                    .prepareStatement("DELETE FROM annuaire WHERE id = ?")) {
+                delete.setInt(1, obj.getId());
+                delete.execute();
+                connection.commit();
+                System.out.println("Annuaire supprimé.");
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            try {
+                connection.rollback();
+                System.err.println("La suppression de l'annuaire " + obj.getId() + " a échouée.");
+            } catch (SQLException e1) {
+                System.err.println(e1.getMessage());
+            }
+        } 
+    }
 }
--- src/main/java/JDBC/GroupeDAOJDBC.java
+++ src/main/java/JDBC/GroupeDAOJDBC.java
@@ -11,8 +11,8 @@ import java.sql.Statement;
 
 import org.apache.derby.shared.common.error.DerbySQLIntegrityConstraintViolationException;
 
-import DAO.DAO;
 import mou.*;
+import DAO.*;
 
 public class GroupeDAOJDBC extends DAO<Groupe> {
     
@@ -93,11 +93,273 @@ public class GroupeDAOJDBC extends DAO<Groupe> {
             insertAppartenir.setInt(2, obj.getId());
             insertContenir.setInt(2, obj.getId());
 
-           
+            IterateurComposant ite = obj.iterateur();
+            while (ite.hasNext()) {
+                Composant c = ite.next();
+                if (c instanceof Personnel) {
+                    Personnel p = (Personnel) c;
+                    selectPersonnel.setInt(1, p.getId());
+                    try (ResultSet rs = selectPersonnel.executeQuery()) {
+                        if (!rs.next()) {
+                            PersonnelDAOJDBC.insert(p, conn);
+                        } else {
+                            System.err.println("Personnel " + p.getId()
+                            + " existe déjà.");
+                        }
+                    }
+                    insertAppartenir.setInt(1, p.getId());
+                    insertAppartenir.execute();
+                } else if (c instanceof Groupe) {
+                    Groupe g = (Groupe) c;
+                    selectGroupe.setInt(1, g.getId());
+                    try (ResultSet rs = selectGroupe.executeQuery()) {
+                        if (!rs.next()) {
+                            GroupeDAOJDBC.insert(g, conn);
+                        } else {
+                            System.err.println("Groupe " + g.getId()
+                            + " existe déjà.");
+                        }
+                    }
+                    insertContenir.setInt(1, g.getId());
+                    insertContenir.execute();
                 }
             }
-        
+        }
     }
 
-   
+    @Override
+    public Groupe create(final Groupe obj) {
+        try {
+            createTables(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        Groupe created = null;
+        try {
+            connection.setAutoCommit(false);
+            insert(obj, connection);
+            connection.commit();
+            created = obj;
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            try {
+                connection.rollback();
+                System.err.println("L'insertion du Groupe " + obj.getId() + " a échouée.");
+            } catch (SQLException e1) {
+                System.err.println(e1.getMessage());
+            }
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+            } catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+
+        return obj;
+    }
+
+    static Groupe read(final int idGroupe, final Connection conn)
+            throws SQLException {
+        Groupe g = null;
+        try (PreparedStatement selectGroupe =
+                conn.prepareStatement("Select * from Groupe WHERE id = ?")) {
+            selectGroupe.setInt(1, idGroupe);
+            try (ResultSet rs = selectGroupe.executeQuery()) {
+                if (rs.next()) {
+                    g = new Groupe(idGroupe, rs.getString("nom"));
+                } else {
+                    return null;
+                }
+            }
+        }
+        try (PreparedStatement selectAppartenir = conn.prepareStatement(
+                "Select * from Appartenir WHERE id_groupe = ?")) {
+            selectAppartenir.setInt(1, idGroupe);
+            try (ResultSet rs = selectAppartenir.executeQuery()) {
+                while (rs.next()) {
+                    Personnel p = PersonnelDAOJDBC
+                            .read(rs.getInt("id_personnel"), conn);
+                    if (p != null) {
+                        g.add(p);
+                    }
+                }
+            }
+        }
+        try (PreparedStatement selectContenir = conn.prepareStatement(
+                "Select * from contenir WHERE id_groupe_contenant = ?")) {
+            selectContenir.setInt(1, idGroupe);
+            try (ResultSet rs = selectContenir.executeQuery()) {
+                while (rs.next()) {
+                    Groupe gFils = GroupeDAOJDBC
+                            .read(rs.getInt("id_groupe_contenu"), conn);
+                    if (gFils != null) {
+                        g.add(gFils);
+                    }
+                }
+            }
+        }
+        return g;
+    }
+    
+    @Override
+    public Groupe find(final String id) {
+        try {
+            return GroupeDAOJDBC.read(Integer.parseInt(id), connection);
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return null;
+    }
+
+    static boolean modify(final Groupe obj, final Connection conn,
+            final boolean insert) throws SQLException {
+        try (PreparedStatement selectGroupe =
+                conn.prepareStatement("SELECT * FROM groupe WHERE id = ?")) {
+            selectGroupe.setInt(1, obj.getId());
+            try (ResultSet rs = selectGroupe.executeQuery()) {
+                if (!rs.next()) {
+                    if (insert) {
+                        try (PreparedStatement insertGroupe =
+                                conn.prepareStatement(
+                                        "insert into groupe values (?, ?)")) {
+                            insertGroupe.setInt(1, obj.getId());
+                            insertGroupe.setString(2, obj.getNom());
+                            insertGroupe.execute();
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        try (PreparedStatement updateGroupe = conn
+                .prepareStatement("UPDATE groupe SET nom = ? WHERE id = ?")) {
+            updateGroupe.setString(1, obj.getNom());
+            updateGroupe.setInt(2, obj.getId());
+            updateGroupe.execute();
+        }
+
+        try (PreparedStatement deleteAppartenir = conn.prepareStatement(
+                "DELETE from appartenir WHERE id_groupe = ?")) {
+            deleteAppartenir.setInt(1, obj.getId());
+            deleteAppartenir.execute();
+        }
+        try (PreparedStatement deleteContenir = conn.prepareStatement(
+                "DELETE from contenir WHERE id_groupe_contenu = ? "
+                        + "OR id_groupe_contenant = ?")) {
+            deleteContenir.setInt(1, obj.getId());
+            deleteContenir.setInt(2, obj.getId());
+            deleteContenir.execute();
+        }
+
+        IterateurComposant ite = obj.iterateur();
+        try (PreparedStatement insertAppartenir =
+                conn.prepareStatement("insert into appartenir values (?, ?)");
+                PreparedStatement insertContenir = conn.prepareStatement(
+                        "insert into contenir values (?, ?)");) {
+            insertAppartenir.setInt(2, obj.getId());
+            insertContenir.setInt(2, obj.getId());
+            while (ite.hasNext()) {
+                Composant c = ite.next();
+                if (c instanceof Personnel) {
+                    Personnel p = (Personnel) c;
+                    PersonnelDAOJDBC.modify(p, conn, true);
+                    insertAppartenir.setInt(1, p.getId());
+                    insertAppartenir.execute();
+                } else if (c instanceof Groupe) {
+                    Groupe g = (Groupe) c;
+                    GroupeDAOJDBC.modify(g, conn, true);
+                    insertContenir.setInt(1, g.getId());
+                    insertContenir.execute();
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public Groupe update(final Groupe obj) {
+        Groupe updated = null;
+        try {
+            connection.setAutoCommit(false);
+            if (modify(obj, connection, false)) {
+                updated = obj;
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            try {
+                connection.rollback();
+                System.err.println("La mise à jour du Groupe " + obj.getId() + " a échouée.");
+            } catch (SQLException e1) {
+                System.err.println(e1.getMessage());
+            }
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+            } catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        return updated;
+    }
+
+    @Override
+    public void delete(final Groupe obj) {
+        try {
+            connection.setAutoCommit(false);
+            try (PreparedStatement deleteAppartenir = connection.prepareStatement(
+                    "DELETE from appartenir WHERE id_groupe = ?")) {
+                deleteAppartenir.setInt(1, obj.getId());
+                deleteAppartenir.execute();
+                System.out.println(
+                        "Groupe " + obj.getId() + " supprimé.");
+            } 
+            try (PreparedStatement deleteContenir = connection.prepareStatement(
+                    "DELETE from contenir WHERE id_groupe_contenu = ?"
+                            + " OR id_groupe_contenant = ?")) {
+                deleteContenir.setInt(1, obj.getId());
+                deleteContenir.setInt(2, obj.getId());
+                deleteContenir.execute();
+                System.out.println(
+                        "Groupe "
+                                + obj.getId() + " supprimé.");
+            }
+            if (tableExists("Annuaire", connection)) {
+                try (PreparedStatement deleteAnnuaire =
+                        connection.prepareStatement(
+                                "DELETE FROM Annuaire "
+                                        + "WHERE racine_groupe = ?")) {
+                    deleteAnnuaire.setInt(1, obj.getId());
+                    deleteAnnuaire.execute();
+                }
+            }
+            System.out.println(
+                    "Annuaire " + obj.getId() + " supprimé.");
+
+            try (PreparedStatement deleteGroupe = connection
+                    .prepareStatement("DELETE from groupe WHERE id = ?")) {
+                deleteGroupe.setInt(1, obj.getId());
+                deleteGroupe.execute();
+            }
+            connection.commit();
+            System.out.println("Groupe " + obj.getId() + " supprimé.");
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            try {
+                connection.rollback();
+                System.err.println("La suppression du Groupe " + obj.getId() + " a échouée.");
+            } catch (SQLException e1) {
+                System.err.println(e1.getMessage());
+            }
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+            } catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
 }
